d98ce1c5a8a098487eb5d46862078309d283fa74
KonekoKawai <69164074+KonekoKawai@users.noreply.github.com>
2025-04-29 19:26:34 +0500

    Update config.h

--- Бот Арсагеры TG - C++/config.h
+++ Бот Арсагеры TG - C++/config.h
@@ -12,7 +12,7 @@ using std::endl;
 class requests;
 
 string chat_id = "-1002584000983"; // -1002351013699
-string bot_token = "8189937701:AAEs0Zgi97TtPb1XO60t6yx6T9DxBpnZY24";
+string bot_token = "XXX"; // РІ GITHUB СЃРєСЂС‹С‚ 
 
 
 string URL_Arsagera = "https://arsagera.ru/api/v1/funds/fa/fund-metrics/";
@@ -21,5 +21,3 @@ string URL_ClockWorld = "http://worldclockapi.com/api/json/utc/now";
 string URL_WorldTime = "https://worldtimeapi.org/api/timezone/UTC/";
 
 string URL_TelegramBOT = "https://api.telegram.org/bot";
-
-// https://api.telegram.org/bot8189937701:AAEs0Zgi97TtPb1XO60t6yx6T9DxBpnZY24/sendMessage?chat_id=-1002351013699&text=fdsfsdfsfsdfs&parse_mode=HTML Написать в тегешку
